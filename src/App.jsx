5935bcee7c4a562a6572198c369d0565bee422ef
CANELA LEDEZMA <49193970@ort>
2026-06-03 10:26:19 -0300

    app empezada

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,27 @@ function App() {
   return (
     <div className="app">
       <h1>Clon de Instagram</h1>
+      <div className="sidebar">
+        <h2>Sidebar</h2>
+      </div>
+
+      <div className="contenido-principal">
+        <div className="header">
+         <h2>Header</h2> 
+        </div>
+
+        <div className="historias">
+         <h2>Historias</h2> 
+        </div>
+
+        <div className="feed">
+          <h2>Feed</h2>
+          
+        </div>
+      </div>
     </div>
   );
 }//lo q se va a ver
+
 
 export default App;

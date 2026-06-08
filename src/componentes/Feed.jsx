a0c130a4b0ef305472c7f625ea6c0b821fc60f71
Android Studio <devandroid@ort>
2026-06-08 10:08:43 -0300

    importe Publicacion en Feed

--- src/componentes/Feed.jsx
+++ src/componentes/Feed.jsx
@@ -1,7 +1,11 @@
+import Publicacion from './Publicacion';
+
 function Feed (){
     return (
     <div className="feed">
       <h2>Feed</h2>
+      {/* Muestro una publicación del feed */}
+      <Publicacion/>
     </div>
     );
 }
---
+++ src/componentes/Publicacion.jsx
@@ -0,0 +1,11 @@
+function Publicacion() {
+  return (
+    <div className="publicacion">
+        
+      <h3>Usuario</h3> {/* aca va a ir el nombre del usuario de la publicación */}
+      <p>Acá va una publicación</p> {/* aca va a ir la imagen de la publicación */}
+    </div>
+  );
+}
+
+export default Publicacion;

b801aa0fbd72b41ff875760452f29017959c01dd
Android Studio <devandroid@ort>
2026-06-08 10:38:48 -0300

    Agrego imagen a publicacion

--- src/componentes/Publicacion.jsx
+++ src/componentes/Publicacion.jsx
@@ -1,9 +1,11 @@
-function Publicacion({usuario, descripcion}) {
+function Publicacion({ usuario, descripcion, imagen }) {
   return (
     <div className="publicacion">
-        
-      <h3>{usuario}</h3> {/* aca va  el nombre del usuario de la publicación */}
-      <p>{descripcion}</p> {/* aca va  la imagen de la publicación */}
+      <h3>{usuario}</h3> {/* aca va el nombre del usuario de la publicación */}
+
+      <img src={imagen} alt="Publicación" /> {/* aca va la imagen de la publicación */}
+
+      <p>{descripcion}</p> {/* aca va la descripción de la publicación */}
     </div>
   );
 }

aa1c02284138bf35f996f7670367daf163f94285
CANELA LEDEZMA <49193970@ort>
2026-06-10 12:58:22 -0300

    Agrego Me gusta en el detalle de publicacion

--- src/componentes/DetallePublicacion.jsx
+++ src/componentes/DetallePublicacion.jsx
@@ -1,4 +1,25 @@
+import { useState } from 'react';
+
 function DetallePublicacion({ publicacion, cerrarDetalle }) {
+  // Guardo si la publicación tiene "Me gusta"
+  const [meGusta, setMeGusta] = useState(false);
+
+  function cambiarMeGusta() {
+    if (meGusta === false) {
+      setMeGusta(true);
+    } else {
+      setMeGusta(false);
+    }
+  }
+
+  function mostrarCorazon() {
+    if (meGusta === true) {
+      return '❤️';
+    }
+
+    return '🤍';
+  }
+
   return (
     <div className="detalle-publicacion">
       <button onClick={cerrarDetalle}>Cerrar</button>
@@ -13,6 +34,10 @@ function DetallePublicacion({ publicacion, cerrarDetalle }) {
       <h3>Persona</h3>
       <p>Descripción de la publicación</p>
       <p>100 Me gusta</p>
+
+      <button onClick={cambiarMeGusta}>
+        {mostrarCorazon()}
+      </button>
     </div>
   );
 }

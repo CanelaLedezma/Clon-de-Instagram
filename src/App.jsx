ea4df718aabf3df466eaa6590f3589c12837543f
CANELA LEDEZMA <49193970@ort>
2026-06-10 08:27:02 -0300

    preparo App para  imagenes de la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css'; // lo conecto con su css
 //lo conecto con lpos componentes:
 import Sidebar from './componentes/Sidebar';
@@ -6,6 +8,7 @@ import Historias from './componentes/Historias';
 import Feed from './componentes/Feed';
 
 function App() {
+  const [imagenes, setImagenes] = useState([]);
   return ( // lo q se va a ver en la pantalla
     <div className="app">
       <h1>Clon de Instagram</h1>
--- src/componentes/Feed.jsx
+++ src/componentes/Feed.jsx
@@ -1,35 +1,41 @@
 import Publicacion from './Publicacion';
 
-function Feed (){
- const publicaciones = [
+function Feed() {
+  const publicaciones = [
     {
       id: 1,
       usuario: 'Persona1',
       descripcion: 'publicación del feed 1',
+      imagen: 'https://placekitten.com/400/400',
     },
     {
       id: 2,
       usuario: 'Persona2',
       descripcion: 'publicación del feed 2',
+      imagen: 'https://placekitten.com/401/400',
     },
     {
       id: 3,
       usuario: 'Persona3',
       descripcion: 'publicación del feed 3',
+      imagen: 'https://placekitten.com/400/401',
     },
-  ];    
-  
- return (
+  ];
+
+  return (
     <div className="feed">
       <h2>Feed</h2>
 
-      {publicaciones.map((publicacion) => (  <Publicacion
+      {publicaciones.map((publicacion) => (
+        <Publicacion
           key={publicacion.id}
           usuario={publicacion.usuario}
           descripcion={publicacion.descripcion}
+          imagen={publicacion.imagen}
         />
       ))}
     </div>
   );
 }
+
 export default Feed;

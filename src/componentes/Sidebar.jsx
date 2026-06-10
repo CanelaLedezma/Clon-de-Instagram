6908110c2e70e7fd073f8062dfec0ead2fc3f05f
CANELA LEDEZMA <49193970@ort>
2026-06-10 13:10:31 -0300

    Agrego foto al perfil del sidebar

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -6,12 +6,13 @@ function Sidebar({ mostrarFeed, mostrarPerfil }) {
       <h2>Sidebar</h2>
 
       {/* Agregué información de ejemplo para simular un usuario */}
-      <TarjetaPerfil
-        nombre="Canela Ledezma"
-        usuario="@canela"
-        seguidores="1200"
-        seguidos="350"
-      />
+   <TarjetaPerfil
+  nombre="Canela Ledezma"
+  usuario="@canela"
+  seguidores="1200"
+  seguidos="350"
+  foto="https://cdn2.thecatapi.com/images/Z5Wd0A-r_.jpg"
+/>
 
       <button onClick={mostrarFeed}>Feed</button>
       <button onClick={mostrarPerfil}>Perfil</button>
--- src/componentes/TarjetaPerfil.jsx
+++ src/componentes/TarjetaPerfil.jsx
@@ -1,6 +1,11 @@
-function TarjetaPerfil({ nombre, usuario, seguidores, seguidos }) {
-  return (
+function TarjetaPerfil({ nombre, usuario, seguidores, seguidos, foto }) {
+    return (
     <div className="tarjeta-perfil">
+      <img
+  className="foto-perfil"
+  src={foto}
+  alt="Foto del perfil"
+/>
       <h3>{nombre}</h3>
       <p>{usuario}</p>
       <p>Seguidores: {seguidores}</p>

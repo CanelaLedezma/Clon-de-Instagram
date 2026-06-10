f21c3e7a2c5571b8cbcd09dc4814b19b13d6a974
CANELA LEDEZMA <49193970@ort>
2026-06-10 10:09:46 -0300

    Agrego vista individual de publicaciones

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,53 @@ import Sidebar from './componentes/Sidebar';
 import Header from './componentes/Header';
 import Historias from './componentes/Historias';
 import Feed from './componentes/Feed';
+import DetallePublicacion from './componentes/DetallePublicacion';
 
 function App() {
   const [imagenes, setImagenes] = useState([]);
   const [publicacionSeleccionada, setPublicacionSeleccionada] = useState(null);
+
   useEffect(() => {
-  async function obtenerImagenes() {
-    const respuesta = await axios.get(
-      'https://api.thecatapi.com/v1/images/search?limit=10'
+    async function obtenerImagenes() {
+      const respuesta = await axios.get(
+        'https://api.thecatapi.com/v1/images/search?limit=10'
+      );
+
+      setImagenes(respuesta.data);
+    }
+
+    obtenerImagenes();
+  }, []);
+
+  function mostrarContenido() {
+    if (publicacionSeleccionada) {
+      return (
+        <DetallePublicacion
+          publicacion={publicacionSeleccionada}
+          cerrarDetalle={() => setPublicacionSeleccionada(null)}
+        />
+      );
+    }
+
+    return (
+      <Feed
+        imagenes={imagenes}
+        seleccionarPublicacion={setPublicacionSeleccionada}
+      />
     );
-
-    setImagenes(respuesta.data);
   }
-
-  obtenerImagenes();
-}, []);
 
   return ( // lo que se va a ver en la pantalla
     <div className="app">
       <h1>Clon de Instagram</h1>
+
       <Sidebar />
       <Header />
       <Historias />
-     <Feed
-  imagenes={imagenes}
-  seleccionarPublicacion={setPublicacionSeleccionada}
-/>
 
-
-<Feed
-  imagenes={imagenes}
-  seleccionarPublicacion={setPublicacionSeleccionada}
-/>
-      </div>
+      {/* muestro el feed o el detalle según haya una publicación seleccionada */}
+      {mostrarContenido()}
+    </div>
   );
 }
 

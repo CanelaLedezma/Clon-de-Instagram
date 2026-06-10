7179c9ede9b3b250c886cbcf41b9a1cbbc1ad91c
CANELA LEDEZMA <49193970@ort>
2026-06-10 10:17:56 -0300

    Agrego perfil de usuario en sidebar

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,8 +1,18 @@
-function Sidebar (){
-    return (
+import TarjetaPerfil from './TarjetaPerfil';
+
+function Sidebar() {
+  return (
     <div className="sidebar">
       <h2>Sidebar</h2>
+
+      <TarjetaPerfil //agregue informacion de ejemplo para simular un usuario y su informacion
+        nombre="Canela Ledezma"
+        usuario="@canela"
+        seguidores="1200"
+        seguidos="350"
+      />
     </div>
-    );
+  );
 }
+
 export default Sidebar;
